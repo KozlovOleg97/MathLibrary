c0deb871fadbfe64506871a44701a0e128ca5a1b
Oleg Kozlov <kozlovoleg2021@gmail.com>
2024-10-10 16:00:05 +0300

    Added Tests for Circle and Triangle

---
+++ MathLib.Tests/ShapeTests/CircleTests.cs
@@ -0,0 +1,27 @@
+﻿using MathLib.Models;
+
+namespace MathLib.Tests.ShapeTests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void Circle_CalculateArea_ShouldReturnCorrectArea()
+        {
+            double radius = 5;
+            var circle = new Circle(radius);
+            double expectedArea = Math.PI * radius * radius;
+
+            var actualArea = circle.CalculateArea();
+
+            Assert.Equal(expectedArea, actualArea, precision: 5);
+        }
+
+        [Fact]
+        public void Circle_CalculateArea_ShouldThrowArgumentException_WhenRadiusIsNegative()
+        {
+            double negativeRadius = -5;
+
+            Assert.Throws<ArgumentException>(() => new Circle(negativeRadius));
+        }
+    }
+}
---
+++ MathLib.Tests/ShapeTests/TriangleTests.cs
@@ -0,0 +1,58 @@
+﻿using MathLib.Models;
+
+namespace MathLib.Tests.ShapeTests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void Triangle_CalculateArea_ShouldReturnCorrectArea()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            var triangle = new Triangle(sideA, sideB, sideC);
+            double semiPerimeter = (sideA + sideB + sideC) / 2;
+            double expectedArea = Math.Sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+
+            var actualArea = triangle.CalculateArea();
+
+            Assert.Equal(expectedArea, actualArea, precision: 5);
+        }
+
+        [Fact]
+        public void Triangle_CalculateArea_ShouldThrowArgumentException_WhenSidesAreInvalid()
+        {
+            double sideA = 1;
+            double sideB = 2;
+            double sideC = 10; 
+
+            Assert.Throws<ArgumentException>(() => new Triangle(sideA, sideB, sideC));
+        }
+
+        [Fact]
+        public void Triangle_IsRightTriangle_ShouldReturnTrue_ForRightTriangle()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5; 
+            var triangle = new Triangle(sideA, sideB, sideC);
+
+            var isRightTriangle = triangle.IsRightTriangle();
+
+            Assert.True(isRightTriangle);
+        }
+
+        [Fact]
+        public void Triangle_IsRightTriangle_ShouldReturnFalse_ForNonRightTriangle()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 6; 
+            var triangle = new Triangle(sideA, sideB, sideC);
+
+            var isRightTriangle = triangle.IsRightTriangle();
+
+            Assert.False(isRightTriangle);
+        }
+    }
+}
